8643db5fb6f7fa632838c7f02ab0cc97ec5e3971
remtori <lqvu99@gmail.com>
2021-02-06 22:19:07 +0700

    lib: fix console_log::init, unwrap instead of expect

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,23 @@ pub mod circuit;
 cfg_if! {
     if #[cfg(feature = "console_log")] {
         fn init_log(log_level: log::Level) {
-            console_log::init_with_level(log_leve).expect("error initializing log");
+            console_log::init_with_level(log_level).unwrap();
         }
     } else {
         fn init_log(log_level: log::Level) {
             use simplelog::*;
-            TermLogger::init(log_level.to_level_filter(), Config::default(), TerminalMode::Stdout).expect("error initializing log");
+            TermLogger::init(log_level.to_level_filter(), Config::default(), TerminalMode::Stdout).unwrap();
         }
     }
 }
 
 #[wasm_bindgen]
 pub fn initialize(log_level: &str) {
-    init_log(log::Level::from_str(log_level).expect("error initializing log: invalid log level"));
+    match log::Level::from_str(log_level) {
+        Ok(level) => init_log(level),
+        Err(_) => {
+            init_log(log::Level::Debug);
+            log::error!("Invalid log level {}", log_level);
+        }
+    }
 }
